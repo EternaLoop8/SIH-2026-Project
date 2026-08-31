73643f05a436fda45e85573f636d74ddcc64a410
EternaLoop8 <soumyaperso06@gmail.com>
2026-08-31 23:36:34 +0530

    Minor fixes

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.DEV
+  ? "http://localhost:3000/api"
+  : "https://sih-2026-project-4le9.vercel.app/api",
   headers: {
     "Content-Type": "application/json",
   },
